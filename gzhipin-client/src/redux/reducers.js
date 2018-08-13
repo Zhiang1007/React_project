5a6a38e63c0dc4c92cd769bc9ca201965d9bf45a
Zhi Ang <1490644301@qq.com>
2018-08-13 21:28:44 +0800

    0813

--- gzhipin-client/src/redux/reducers.js
+++ gzhipin-client/src/redux/reducers.js
@@ -3,27 +3,17 @@
  */
 import {combineReducers} from 'redux'
 
-import {
-    AUTH_SUCCESS,
-    ERROR_MSG
-} from './action-types'
-
-const initUser = {
-    username: '', // 用户名
-    type: '', // 类型
-    msg: '', // 错误提示信息
-    redirectTo: '' // 需要自动跳转的路由path
+const initUser= {
+    usrename: "",
+    type: "",
 }
-function user(state = initUser, action) {
-    switch (action.type) {
-        case AUTH_SUCCESS: // 认证成功
-            return {...action.data, redirectTo: '/'}
-        case ERROR_MSG: // 错误信息提示
-            return {...state, msg: action.data}
-        default:
-            return state
+function user (state=initUser,action){
+   switch (action.type){
+       default:
+           return state
     }
 }
+
 
 // 返回合并的reducer
 export default combineReducers({
